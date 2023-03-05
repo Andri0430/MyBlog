ec84292dfe9d6a394f18d00ba7eb75cf975b80e5
andri <andri@LAPTOP-6FVFR528>
2023-03-05 22:23:10 +0700

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace MyBlog.Controllers
+{
+    [Authorize]
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            var claim = HttpContext.User.Claims;
+            var userFullName = claim.Where(x => x.Type == "name").FirstOrDefault()?.Value;
+
+            ViewData["FullName"] = userFullName;
+            return View();
+        }
+    }
+}
---
+++ Migrations/20230305132653_Table_User.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace MyBlog.Migrations
+{
+    /// <inheritdoc />
+    public partial class TableUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Username = table.Column<string>(type: "longtext", nullable: true),
+                    Password = table.Column<string>(type: "longtext", nullable: true),
+                    FullName = table.Column<string>(type: "longtext", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
